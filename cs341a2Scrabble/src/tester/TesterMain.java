6717b49b4ad267363617e816f209a5f06158d6a1
Taj Okonkwo <taj_okonkwo@pixpando.com>
2026-09-15 21:10:30 -0700

    Created test main class in tester.

---
+++ cs341a2Scrabble/src/tester/TesterMain.java
@@ -0,0 +1,15 @@
+package tester;
+
+/**
+ * A test main class
+ * @author Taj Okonkwo
+ * @version 1.0
+ */
+public class TesterMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
